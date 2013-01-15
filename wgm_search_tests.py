fa5bedee826dc700ee88791a88dffe98f9963289
Sascha Schneider <development@suntsu.org>
2013-01-15 04:25:44 +0100

    First (failed) attempt of testing WTForms validator - non-existent ZIP

--- wgm_search_tests.py
+++ wgm_search_tests.py
@@ -20,5 +20,10 @@ class WGMSearchTestCase(unittest.TestCase):
         rv = self.app.get('/list')
         assert 'Tut uns leid' in rv.data
 
+    def test_wrong_zip(self):
+        rv = self.app.get('/', data=dict(zip='47225', distance=5, submit="Search"), follow_redirects=True)
+        print rv.data
+        assert 'existiert nicht' in rv.data
+
 if __name__ == '__main__':
     unittest.main()
